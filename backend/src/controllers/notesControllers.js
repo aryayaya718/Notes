97436bda86320d1efc0503d52d4a2054a795aa78
aryayaya718 <jainarya846@gmail.com>
2025-12-21 19:12:22 +0530

    completed backend

--- backend/src/controllers/notesControllers.js
+++ backend/src/controllers/notesControllers.js
@@ -1,24 +1,65 @@
 import Note from "../models/note.js";
 export async function getAllNotes(req, res) {
-  try{
-    const data=await Note.find();
+  try {
+    const data = await Note.find();
     res.status(200).json(data);
-  }
-  catch(error){
-    res.status(500).json({message:"server error"});
+  } catch (error) {
+    res.status(500).json({ message: "server error" });
     console.error(error);
   }
 }
-export async function addNote(req,res){
-  try{
-    const{title,content}=req.body;
-    const newNote=new Note({title,content});
-    const savedNote=await newNote.save();
+export async function addNote(req, res) {
+  try {
+    const { title, content } = req.body;
+    const newNote = new Note({ title, content });
+    const savedNote = await newNote.save();
     res.status(201).json(savedNote);
-
+  } catch (error) {
+    res.status(500).json({ message: "server error" });
+    console.error(error);
   }
-  catch(error){
-    res.status(500).json({message:"server error"});
+}
+export async function getOneNote(req, res) {
+  try {
+    const note = await Note.findById(req.params.id);
+    if (!note) {
+      res.status(404).json({ Message: "not found" });
+    } else {
+      res.status(200).json(note);
+    }
+  } catch (error) {
+    res.status(500).json({ message: "server error" });
+    console.error(error);
+  }
+}
+export async function updateNote(req, res) {
+  try {
+    const { title, content } = req.body;
+    const updatedNote = await Note.findByIdAndUpdate(
+      req.params.id,
+      { title, content },
+      { new: true }
+    );
+    if (!updatedNote) {
+      res.status(404).json({ message: "not found" });
+    } else {
+      res.status(200).json(updatedNote);
+    }
+  } catch (error) {
+    res.status(500).json({ message: "server error" });
+    console.error(error);
+  }
+}
+export async function deleteNote(req, res) {
+  try {
+    const deletedNote = await Note.findByIdAndDelete(req.params.id);
+    if (!deletedNote) {
+      res.status(404).json({ message: "not found" });
+    } else {
+      res.status(200).json(deletedNote);
+    }
+  } catch (error) {
+    res.status(500).json({ message: "server error" });
     console.error(error);
   }
 }
--- backend/src/routes/notesRoute.js
+++ backend/src/routes/notesRoute.js
@@ -1,6 +1,15 @@
 import express from "express";
-import { addNote, getAllNotes } from "../controllers/notesControllers.js";
+import {
+  addNote,
+  getAllNotes,
+  getOneNote,
+  updateNote,
+  deleteNote,
+} from "../controllers/notesControllers.js";
 const router = express.Router();
 router.get("/", getAllNotes);
-router.post("/",addNote);
+router.get("/:id", getOneNote);
+router.post("/", addNote);
+router.put("/:id", updateNote);
+router.delete("/:id", deleteNote);
 export default router;
