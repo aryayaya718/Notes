0641a20a6c0735d87c611643f5bc5e6f41baefa2
aryayaya718 <jainarya846@gmail.com>
2025-12-28 23:32:39 +0530

    developed navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,6 +1,10 @@
+import { Plus } from 'lucide-react';
 function Navbar() {
   return <>
-  <div></div>
+  <div className="Navbar bg-espresso h-25 flex items-center justify-between px-20">
+    <div className="text-taupe font-Valentino text-5xl">Scribblee</div>
+    <div className="" ><button className="btn bg-taupe border-walnut rounded-full"><Plus />Create</button> </div>
+  </div>
   </>;
 }
 export default Navbar;
