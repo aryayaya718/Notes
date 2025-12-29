a2dab9e0d6a2cf72ccc21e97300337b5215b783e
aryayaya718 <jainarya846@gmail.com>
2025-12-29 16:14:39 +0530

    arya

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,9 +1,10 @@
 import { Plus } from 'lucide-react';
+import { Link } from "react-router";
 function Navbar() {
   return <>
-  <div className="Navbar bg-espresso h-25 flex items-center justify-between px-20">
-    <div className="text-taupe font-Valentino text-5xl">Scribblee</div>
-    <div className="" ><button className="btn bg-taupe border-walnut rounded-full"><Plus />Create</button> </div>
+  <div className="Navbar bg-espresso h-25 flex items-center justify-between px-20 fixed w-full">
+    <div className="text-taupe font-Valentino text-5xl"><Link to="/">Scribblee</Link></div>
+    <div className="" ><Link to="/create"><button className="btn bg-taupe border-walnut rounded-full"><Plus />Create</button></Link> </div>
   </div>
   </>;
 }
--- frontend/src/pages/Homepage.jsx
+++ frontend/src/pages/Homepage.jsx
@@ -1,10 +1,20 @@
 import Navbar from "../components/Navbar.jsx";
+import { useState } from 'react';
 function Homepage() {
+  const [Name,setName]=useState("Arya");
+  setName("Deepanshu");
   return (
     <div>
       <Navbar />
-      Arya
+       <div className="bg-parchment min-h-screen">
+      <div className="h-25"></div>
+      <div></div>
+     </div>
     </div>
   );
 }
 export default Homepage;
+
+
+const arr=["j","h"];
+const[a,b]=arr;
