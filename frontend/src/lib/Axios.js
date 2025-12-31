65cfa64a86a43830a31c64e5969897253e73def7
aryayaya718 <jainarya846@gmail.com>
2025-12-31 19:46:43 +0530

    fixed bug

--- frontend/src/lib/Axios.js
+++ frontend/src/lib/Axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001/api/notes" : "/api/notes";
+const baseUrl = import.meta.env.MODE === "development" ? "http://localhost:5001/api/notes" : "/api/notes";
 const api=axios.create({
     baseURL:baseUrl
 });
